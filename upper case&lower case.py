1494373860a79e41170ffbfe611c6c116b615187
bhanureddy123124 <112370442+bhanureddy123124@users.noreply.github.com>
2022-09-28 10:40:37 +0530

    Add files via upload

---
+++ upper case&lower case.py
@@ -0,0 +1,26 @@
+print("enter * to exit")
+a=[]
+c,d,e,f=0,0,0,0
+for i in range(0,100):
+   b=input ("enter the element:") 
+   if(b== '*'):
+       break
+   else:
+      a.append(b)
+for i in a:
+    if(i.isupper()): 
+        c+=1
+    elif(i.islower()):
+        d+=1
+    elif(i.isnumeric()):
+        e+=1
+    else:
+        (i.isdecimal())
+        f+=1
+        
+print("no of lowercase element",d)
+print("no of uppercase element",c) 
+print("no of numbers ",e)
+print("special character ",f)
+
+
