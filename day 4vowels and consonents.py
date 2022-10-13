87435ad603c169eb39ba07872126e24bd321e110
bhanureddy123124 <112370442+bhanureddy123124@users.noreply.github.com>
2022-10-13 11:12:32 +0530

    Add files via upload

---
+++ day 4vowels and consonents.py
@@ -0,0 +1,13 @@
+n=input("enter the string")
+for char in n:
+    if char.lower() in ("aeiouAEIOU"):
+        print("vowels",char)
+    else:
+        char.lower()not in ("aeiouAEIOU")
+        print("consonents",char)
+        
+            
+        
+            
+            
+            
